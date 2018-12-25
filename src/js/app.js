4313d9c32c99966f707c2d774a4a20a52999ab81
opherv <opher@interlude.fm>
2018-12-25 15:57:55 +0200

    test

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 export default function onInit(player, context) {
     // Visit http://developer.helloeko.com for detailed API documentation!
-    console.log('hello!');
+    console.log('hello! testing cache');
 }
